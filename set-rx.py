ab0dd9c2a12c94848aae4a7a2ba026c126576c23
Marcus B <marcusb@mabsoft.org>
2017-01-27 21:52:55 +1030

    Working, v0.9

--- set-rx.py
+++ set-rx.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python
 
-import json, cgi, cgitb
+import json, cgi, cgitb, os
 
 form = cgi.FieldStorage()
 
-json = form.getvalue('x')
+cgi_json = form.getvalue('x')
 
 print "Content-type:text/html\r\n\r\n"
 
-print json
+form = json.loads(cgi_json)
+
+if (form['freq']):
+	freq_hz = int(float(str(float(form['freq'])))) # lots os simple filters to ensure we only get a number!
+	print freq_hz
+	if (freq_hz == 0):
+		os.system("sudo /usr/local/bin/freq_pi -q")
+	else:
+		os.system("sudo /usr/local/bin/freq_pi -f %d" % (freq_hz))
+	
+else:
+	print "No freq"
